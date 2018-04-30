0baf0f8314a2151ed1e5dfbf882fbeeadd2023f5
Theodore Gonzalez <useeless37@yahoo.com>
2018-04-30 15:08:22 +0200

    create AdViewController

---
+++ FLApp/Ad Screen/AdViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FLApp
+//
+//  Created by Theodore Gonzalez on 4/30/18.
+//  Copyright © 2018 wandercodesfze. All rights reserved.
+//
+
+import UIKit
+
+class AdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
