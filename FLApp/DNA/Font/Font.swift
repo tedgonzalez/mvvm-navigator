5b676f9c232445c3f5578f49ebc380fea4ebde0a
Theodore Gonzalez <useeless37@yahoo.com>
2018-04-30 22:16:31 +0200

    add fonts and assets

---
+++ FLApp/DNA/Font/Font.swift
@@ -0,0 +1,19 @@
+//
+//  Font.swift
+//  FLApp
+//
+//  Created by Theodore Gonzalez on 4/30/18.
+//  Copyright © 2018 wandercodesfze. All rights reserved.
+//
+
+import Foundation
+enum FontType: String {
+    case semiBold = "Montserrat-SemiBold"
+    case ultraLight = "Montserrat-UltraLight"
+    case bold = "Montserrat-Bold"
+    case extraBold = "Montserrat-ExtraBold"
+    case hairline = "Montserrat-Hairline"
+    case light = "Montserrat-Light"
+    case regular = "Montserrat-Regular"
+    case black = "Montserrat-Black"
+}
